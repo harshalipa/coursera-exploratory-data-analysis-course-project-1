1a3344ca3875ff502a6b5a8526fcb1b481910d7f
harshalipa <87533129+harshalipa@users.noreply.github.com>
2021-08-07 15:42:15 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,9 +1,27 @@
-dataFile <- "./data/household_power_consumption.txt"
-data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+library("data.table")
 
-#str(subSetData)
-globalActivePower <- as.numeric(subSetData$Global_active_power)
+
+
+#Reads in data from file then subsets data for specified dates
+powerDT <- data.table::fread(input = "household_power_consumption.txt"
+                            , na.strings="?")
+
+# Prevents histogram from printing in scientific notation
+powerDT[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+
+# Change Date Column to Date Type
+powerDT[, Date := lapply(.SD, as.Date, "%d/%m/%Y"), .SDcols = c("Date")]
+
+
+# Filter Dates for 2007-02-01 and 2007-02-02
+powerDT <- powerDT[(Date >= "2007-02-01") & (Date <= "2007-02-02")]
+
 png("plot1.png", width=480, height=480)
-hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+
+ #Plot 1
+hist(powerDT[,powerDT$Global_active_power], main="Global Active Power", 
+    xlab="Global Active Power (kilowatts)", ylab="Frequency", col="red")
+
 dev.off()
+
+
