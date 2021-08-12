7a7d0479dd886d86ff971484c266fcea998aff9d
harshalipa <87533129+harshalipa@users.noreply.github.com>
2021-08-12 16:06:58 +0530

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,15 @@
+library("data.table")
+power <- data.table::fread(input = "household_power_consumption.txt")
+
+power[, Global_active_power := lapply(.SD, as.numeric)
+      ,.SDcols = c("Global_active_power")]
+
+png("plot1.png", width=480, height=480)
+
+hist(power[,power$Global_active_power], 
+     main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", 
+     ylab="Frequency", col="red")
+
+dev.off()
+
